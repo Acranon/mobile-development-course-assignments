97e0cd850e63d27ec3cb94c585fbe734f8a2cb87
Sean Cormier <Sean Cormier>
2017-02-20 18:21:12 -0600

    failing at angular

---
+++ 03-angular/assignment-1/src/app/Components/student.component.ts
@@ -0,0 +1,96 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'student',
+    templateUrl: 'student.component.html',
+})
+export class StudentComponent {
+    studentName: string;
+    studentEmail: string;
+    pointsScored: number;
+    pointsPossible: number;
+    percent: number;
+    iPercent: number;
+    iGrade: string;
+    grade: string;
+    iName: string;
+    iScoredPoints: number;
+    iPossiblePoints: number;
+    assignmentList: Assignment[];
+
+    constructor() {
+        this.studentName = "Bob Smith";
+        this.studentEmail = "bobsmith@gmail.com";
+        this.pointsScored = 0;
+        this.pointsPossible = 0;
+        this.percent = 0;
+        this.grade = 'N/A';
+        this.assignmentList = [];
+    }
+
+    addAssignment(): void {
+        let assignment: Assignment;
+
+        assignment = {
+            name: this.iName,
+            pointsScored: this.iScoredPoints,
+            pointsPossible: this.iPossiblePoints,
+            percent: this.iScoredPoints / this.iPossiblePoints,
+            iGrade: this.getIGrade()
+        }
+        this.assignmentList.push(assignment);
+        this.iPercent = assignment.percent;
+        this.updatePerformance();
+    }
+
+    updatePerformance() {
+        this.pointsPossible = this.assignmentList.reduce(this.addPointsPossible, 0);
+        this.pointsScored = this.assignmentList.reduce(this.addPointsScored, 0);
+        this.percent = this.pointsScored / this.pointsPossible;
+        this.grade = this.getGrade();
+    }
+
+
+    private getGrade(): string {
+        if (this.percent >= 0.9)
+            return 'A';
+        else if (this.percent >= .8)
+            return 'B';
+        else if (this.percent >= .7)
+            return 'C';
+        else if (this.percent >= .6)
+            return 'D';
+        else
+            return 'F';
+    }
+
+    private getIGrade(): string {
+        if (this.iPercent >= 0.9)
+            return 'A';
+        else if (this.iPercent >= .8)
+            return 'B';
+        else if (this.iPercent >= .7)
+            return 'C';
+        else if (this.iPercent >= .6)
+            return 'D';
+        else
+            return 'F';
+    }
+
+    private addPointsPossible(tally: number, assignment: Assignment) {
+        return tally + assignment.pointsPossible;
+    }
+
+    private addPointsScored(tally: number, assignment: Assignment) {
+        return tally + assignment.pointsScored;
+    }
+}
+
+interface Assignment {
+    name: string;
+    pointsScored: number;
+    pointsPossible: number;
+    percent: number;
+    iGrade: string;
+}
--- 03-angular/assignment-1/src/app/app.module.ts
+++ 03-angular/assignment-1/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
+
+import { StudentComponent } from './Components/student.component';
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, StudentComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
